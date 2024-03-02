13ee2a10568c5631afa995c34d2685f5a9eeb203
SHUBHAM SHARMA <shubham.sharma@fargowiz.com>
2024-03-02 12:29:27 +0530

    currection

--- userList2/userList/src/App.jsx
+++ userList2/userList/src/App.jsx
@@ -1,71 +1,37 @@
 import React from "react";
 import "./App.css";
+import { DATA } from "./data.js";
 
-import UserTable from "./components/UserTable.jsx";
+function CoreConcept({ id, title, body }) {
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{title}</td>
+      <td>{body + title}</td>
+    </tr>
+  );
+}
 
 function App() {
-  const userss = [
-    {
-      id: 1,
-      title: "virat",
-      body: "Cricketer",
-    },
-    {
-      id: 2,
-      title: "Dhoni",
-      body: "Cricketer",
-    },
-    {
-      id: 3,
-      title: "Rahul",
-      body: "Cricketer",
-    },
-    {
-      id: 4,
-      title: "abc",
-      body: "cba",
-    },
-    {
-      id: 5,
-      title: "abcd",
-      body: "dcba",
-    },
-    {
-      id: 6,
-      title: "ABCDE",
-      body: "edcba",
-    },
-    {
-      id: 7,
-      title: "abcdef",
-      body: "fedcba",
-    },
-    {
-      id: 8,
-      title: "abcdefg",
-      body: "gfedcba",
-    },
-    {
-      id: 9,
-      title: "abcdefgh",
-      body: "hgfedcba",
-    },
-    {
-      id: 10,
-      title: "abcdefghi",
-      body: "ihgfedcba",
-    },
-    {
-      id: 11,
-      title: "abcdefghj",
-      body: "jhgfedcba",
-    },
-  ];
-
+  const limitedData = DATA.slice(0, 10);
   return (
     <div className="App">
-      <h1>List of Users</h1>
-      <UserTable useres={userss} />
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+          <tbody>
+            {limitedData.map((user) => (
+              <CoreConcept key={user.id} {...user} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ userList2/userList/src/data.js
@@ -0,0 +1,70 @@
+
+
+export const  DATA = 
+      [
+      {
+        id: 1,
+        title: "A",
+        body: 'Body of User  ' ,
+      },
+      {
+        id: 2,
+        title: "B",
+        body: "Body of User  ",
+      },
+      {
+        id: 3,
+        title: "C",
+        body: "Body of User  ",
+      },
+      {
+        id: 4,
+        title: "D",
+        body: "Body of User  ",
+      },
+      {
+        id: 5,
+        title: "E",
+        body: "Body of User  ",
+      },
+      {
+        id: 6,
+        title: "F",
+        body: "Body of User  ",
+      },
+      {
+        id: 7,
+        title: "G",
+        body: "Body of User  ",
+      },
+      {
+        id: 8,
+        title: "H",
+        body: "Body of User  ",
+      },
+      {
+        id: 9,
+        title: "I",
+        body: "Body of User  ",
+      },
+      {
+        id: 10,
+        title: "J",
+        body: "Body of User  ",
+      },
+      {
+        id: 11,
+        title: "K",
+        body: "Body of User  ",
+      },
+      {
+        id: 12,
+        title: "L",
+        body: "Body of User  ",
+      },
+      {
+        id: 13,
+        title: "M",
+        body: "Body of User  ",
+      },
+    ];
